13de71ec54be997cb165efd62553401ac0b1cc8d
CoreTechies <coretechies@CoreTechiess-Mac-mini.local>
2021-08-02 17:33:29 +0530

    write functionality add

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text, Image, SafeAreaView, TouchableOpacity } from 'react-native';
-import NfcManager, { NfcEvents } from 'react-native-nfc-manager';
+import NfcManager, { NfcEvents, Ndef , NfcTech} from 'react-native-nfc-manager';
+
+function buildUrlPayload(valueToWrite) {
+  return Ndef.encodeMessage([
+      Ndef.textRecord(valueToWrite),
+  ]);
+}
 
 class App extends Component {
 
   async componentDidMount() {
     let is_supported = await NfcManager.isSupported()
-    console.log('anss', is_supported)
+    console.log('Supported', is_supported)
+    
+    
     if (is_supported) {
-     
       NfcManager.start();
       NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
-        console.log('tag', tag);
+        console.log('Scan tag response =>', tag);
         //NfcManager.setAlertMessageIOS('I got your tag!');
         NfcManager.unregisterTagEvent().catch(() => 0);
       });
@@ -22,7 +29,10 @@ class App extends Component {
   componentWillUnmount() {
     NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
     NfcManager.unregisterTagEvent().catch(() => 0);
+    
   }
+
+  
 
   _cancel = () => {
     NfcManager.unregisterTagEvent().catch(() => 0);
@@ -38,7 +48,26 @@ class App extends Component {
     }
   }
 
+  _testNdef = async () => {
+    try {
+      let resp = await NfcManager.requestTechnology(NfcTech.Ndef, {
+        alertMessage: 'Ready to write some NFC tags!'
+      });
+      console.log('res',resp);
+      let ndef = await NfcManager.getNdefMessage();
+      console.log('ndef',ndef.ndefMessage[0].payload);
+      let bytes = buildUrlPayload('hello Successfully demo');
+      await NfcManager.writeNdefMessage(bytes);
+      console.log('successfully write ndef');
+      
+    
+    } catch (ex) {
+      console.warn('ex', ex);
+      
+    }
+  }
 
+  
   render() {
     return (
       <SafeAreaView style={{ flex: 1, }}>
@@ -55,6 +84,13 @@ class App extends Component {
           onPress={this._cancel}
         >
           <Text>Cancel Test</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{ padding: 10, width: 200, margin: 20, borderWidth: 1, borderColor: 'black' }}
+          onPress={this._testNdef}
+        >
+          <Text>Write Test</Text>
         </TouchableOpacity>
       </SafeAreaView>
     )
